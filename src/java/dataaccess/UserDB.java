a52a46b628611616829766c65abb5984fc01cbcd
814992 <814992@SAIT224085IT.ACDM.DS.SAIT.CA>
2020-11-29 22:48:10 -0700

    modifyied UserDB
    as above aswell as modified relevant models

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import models.User;
 
 
@@ -13,6 +14,43 @@ public class UserDB {
             return user;
         } finally {
             em.close();
+        }
+    }
+    
+    public void setUserUUID(User user, String UUID){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            user.setResetPasswordUuid(UUID);
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch(Exception e){
+            trans.rollback();
+        }
+    }
+
+    public void update(User user) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch (Exception e) {
+            trans.rollback();
+        }
+    }
+
+    public User getByUUID(String uuid) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            return em.createNamedQuery("User.findByResetPasswordUuid", User.class).setParameter("resetPasswordUuid", uuid).getResultList().get(0);
+        } catch (Exception e) {
+            return null;
         }
     }
 }
--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -105,5 +105,4 @@ public class Role implements Serializable {
     public String toString() {
         return "models.Role[ roleId=" + roleId + " ]";
     }
-    
 }
